bcb7aa7b8525360974d2e293d6d0a0166f2a7da6
mecseridavid <mecserdavid@gmail.com>
2022-09-01 17:40:23 +0200

    Add Makefile, migrations

---
+++ SolarCoffee.Data/Migrations/20220901153304_InitialMigrationFix.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SolarCoffee.Data.Migrations
+{
+    public partial class InitialMigrationFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_CustomerAddress_PrimaryAddressId",
+                table: "Customers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CustomerAddress",
+                table: "CustomerAddress");
+
+            migrationBuilder.RenameTable(
+                name: "CustomerAddress",
+                newName: "CustomerAddresses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CustomerAddresses",
+                table: "CustomerAddresses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_CustomerAddresses_PrimaryAddressId",
+                table: "Customers",
+                column: "PrimaryAddressId",
+                principalTable: "CustomerAddresses",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_CustomerAddresses_PrimaryAddressId",
+                table: "Customers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_CustomerAddresses",
+                table: "CustomerAddresses");
+
+            migrationBuilder.RenameTable(
+                name: "CustomerAddresses",
+                newName: "CustomerAddress");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_CustomerAddress",
+                table: "CustomerAddress",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_CustomerAddress_PrimaryAddressId",
+                table: "Customers",
+                column: "PrimaryAddressId",
+                principalTable: "CustomerAddress",
+                principalColumn: "Id");
+        }
+    }
+}
--- SolarCoffee.Web/Program.cs
+++ SolarCoffee.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SolarCoffee.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<SolarDbContext>(opts => {
+    opts.EnableDetailedErrors();
+    opts.UseNpgsql(builder.Configuration.GetConnectionString("solar.dev"));
+});
 
 var app = builder.Build();
 
